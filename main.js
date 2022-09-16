dcf189f771b1263fe3b17b19e4b9e5b516e4279c
matheus <matheusaltrao2@gmail.com>
2022-09-16 00:45:44 -0400

    add alert copied

--- main.js
+++ main.js
@@ -2,8 +2,22 @@ function copyEmail() {
 
   const valueInput = document.getElementById('input-email')
   const email = valueInput.value
-
+  const alert = document.getElementById('alert')
+  
+ 
   navigator.clipboard.writeText(email)
+
+  alert.style.opacity = 1
+  setTimeout(function () {
+    alert.style.opacity = 0
+  }, 2000)
+
+ 
+ 
+  alert.classList.add('show')
 
   console.log(email)
 }
+
+
+
